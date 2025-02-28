53d8d344253ab231a3757c291c3d7b390e3a36f1
FRL <80878559+fangrouli@users.noreply.github.com>
2025-02-28 13:33:45 +0800

    Add files via upload

---
+++ coremarkDebug/coremark_params.h
@@ -0,0 +1,16 @@
+/*
+To set the compilation flag instead of in Makefile
+*/
+#define ITERATIONS 10
+#define HAS_PRINTF 1
+#define HAS_FLOAT  0
+#define myDebug
+#define CORE_DEBUG 1
+
+#define PERFORMANCE_RUN 1
+#define VALIDATION_RUN  0
+#define PROFILE_RUN     0
+#define TOTAL_DATA_SIZE 2000
+#define SEED_METHOD     2
+
+//0:arg 2: volatile
